a48a6b7526050a1ebbaa6a788ec4ab01b567489b
Reptar-IT <badguy417@gmail.com>
2021-10-15 16:50:30 -0500

    add heroku port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const https = require("https");
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const app = express();
 //let app use express json parser 
 //app.use(express.json());
