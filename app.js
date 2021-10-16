3d2417d67695b8e948f5a2dc696896b1d99aa33c
Reptar-IT <badguy417@gmail.com>
2021-10-16 09:57:22 -0500

    made it better in heroku

--- app.js
+++ app.js
@@ -97,6 +97,6 @@ if(process.env.PORT != null){
 //------------------- START SERVER ----------------//
 
 app.listen(port, function(){
-  console.log("Database_URL", process.env.db_uri);
+  console.log("Database_URL", db_uri);
   console.log("Server is now docked at port " + port);
 });
